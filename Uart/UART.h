7e8dc9854f2a0c714115b39c98d2578cc4c95dee
Jannik Haumann Lading <201707261@post.au.dk>
2018-11-22 21:26:31 +0100

    Done Uart

--- Uart/UART.h
+++ Uart/UART.h
@@ -14,77 +14,17 @@
 class UART
 {
 public:
-  UART()
-  {
-    open_port();
-
-    // Get current serial port configuration
-    tcgetattr(fd, &options);
-
-    // Set baudrate
-    cfsetispeed(&options, B115200);
-    cfsetospeed(&options, B115200);
-
-    // Enable the receiver and set local mode
-    options.c_cflag |= (CLOCAL | CREAD);
-
-    // Set data bits, stopbit and parity
-    options.c_cflag &= ~PARENB; // Disable parity bit with tilde(~)
-    options.c_cflag &= ~CSTOPB; // Disable 2 stop bit with tilde(~)
-    options.c_cflag &= ~CSIZE;  // Mask the character size bits
-    options.c_cflag |= CS8;     // Select 8 data bits
-
-    // Set the new options for the port
-    tcsetattr(fd, TCSANOW, &options);
-
-    // Makes read return 0, if nothing is in buffer, so it doesn't block up program
-    // fcntl(fd, F_SETFL, FNDELAY);
-    fcntl(fd, F_SETFL, 0);
-  }
-
-  char* readChar()
-  {
-    //ioctl(fd, FIONREAD, &bytes);
-    int rdlen = read(fd, buf, 10);
-    buf[rdlen] = '\0';
-//    std::cout << "rdlen: " << rdlen << std::endl;
-
-    if (rdlen > 0)
-      return buf;
-    else
-      return readerr;
-  }
-
-  void writeBinary(char *message, int len)
-  {
-    int n = write(fd, message, len);
-  }
-
-  void writeChar(char *message)
-  {
-    int n = write(fd, message, sizeof(&message));
-    if (n < 0)
-      fputs("write() of 1 byte failed!\n", stderr);
-  }
-
-  ~UART()
-  {
-    close(fd);
-  }
+  UART();
+  char readChar();
+  void writeByte(char message);
+  void writeChar(char message);
+  char receivestatus();
+  void sendCoffeorder(char filter, char waterAmount, char CoffeeNumber, char coffeeAmount);
+  ~UART();
 private:
-  void open_port()
-  {
-    fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
-
-    if (fd == -1) //Could not open the port.
-      perror("open_port: Unable to open /dev/ttyS0 - ");
-    else
-      fcntl(fd, F_SETFL, 0);
-  }
-
+  void open_port();
   int bytes;
   int fd; //File desciptor
   char buf[10];
   struct termios options;
-  char readerr[5] = "fejl";
 };
--- Uart/testuart.cpp
+++ Uart/testuart.cpp
@@ -6,15 +6,14 @@
 int main(int argc, char const *argv[]) {
 
 	UART uart;
-	char message[] = "Hej";
+	char message = 'H';
 
 	//char msg[] = {0x01};
 
 	while (1) {
-		uart.writeChar(message);
-		//uart.writeBinary(msg, 1);
-		printf("%s\n", uart.readChar());
-		//std::cout << uart.readChar() << std::endl;
+
+		std::cout << "Status er: " << uart.receivestatus() << std::endl;
+		uart.sendCoffeorder('7', 30, '1', 14);
 	}
 
 	return 0;
