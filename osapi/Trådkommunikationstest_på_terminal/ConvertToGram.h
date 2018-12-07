89130b126011eda5668332cbe61f90643af520a2
4p0l1o <apollotoby@gmail.com>
2018-12-07 14:10:01 +0100

    Traadkom tested with terminal as webApp simulation

---
+++ osapi/Trådkommunikationstest_på_terminal/ConvertToGram.h
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+class ConvertToGram
+{
+public:
+  ConvertToGram();
+  int getGrams(int, char);
+
+private:
+  double strength_=0;
+  double grams_=0;
+};
---
+++ osapi/Trådkommunikationstest_på_terminal/T2_coffeeOrderHandler.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <osapi/Thread.hpp>
+#include <osapi/ThreadFunctor.hpp>
+#include "Messages.hpp"
+#include <iostream>
+#include <string>
+#include <osapi/Utility.hpp>
+#include "UART.h"
+#include "ConvertToGram.h"
+#include "MsgQueue.h"
+
+class T2_coffeOrderHandler :public osapi::ThreadFunctor
+{
+public:
+  T2_coffeOrderHandler(int size, UART* u);
+  virtual void run();
+  void handler(osapi::Message* msg, unsigned long id);
+  void sendStatus();
+  void setmsgQ(MsgQueue *msgQ);
+  osapi::MsgQueue* getmsgQ();
+
+private:
+  osapi::MsgQueue*  mq_;
+  MsgQueue*  T1Mq_;
+  UART*              UART_;
+  ConvertToGram     grams_;
+  char               filter_='0';
+  status            status_;
+};
